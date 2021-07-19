a2c9eb3418c804999497495051172315b95b0dbb
wadhah101 <wadhah.mahroug15@gmail.com>
2021-07-19 13:22:54 +0100

    added  reveal next button

--- src/components/charts/position.tsx
+++ src/components/charts/position.tsx
@@ -1,17 +1,18 @@
 import * as React from "react";
 import { VoteTotal } from "src/types/vote";
 import { Bar } from "react-chartjs-2";
+import clsx from "clsx";
 
 interface IPositionChartProps {
     data: VoteTotal;
 }
 
-const data = {
-    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+const makeChartData = (data: VoteTotal) => ({
+    labels: Object.keys(data.count),
     datasets: [
         {
             label: "# of Votes",
-            data: [12, 19, 3, 5, 2, 3],
+            data: Object.values(data.count),
             backgroundColor: [
                 "rgba(255, 99, 132, 0.2)",
                 "rgba(54, 162, 235, 0.2)",
@@ -31,24 +32,78 @@ const data = {
             borderWidth: 1,
         },
     ],
-};
+});
 
 const options = {
     scales: {
-        yAxes: [
+        y: [
             {
-                ticks: {
-                    beginAtZero: true,
-                },
+                min: 0,
+                max: 100,
+                // ticks: {
+                //     beginAtZero: true,
+                // },
             },
         ],
     },
 };
 
-const PositionChart: React.FunctionComponent<IPositionChartProps> = () => {
+const PositionChart: React.FunctionComponent<IPositionChartProps> = ({
+    data,
+}) => {
+    const [currentIndex, setCurrentIndex] = React.useState(0);
+    const resetedData = {
+        ...data,
+        count: Object.fromEntries(
+            Object.entries(data.count).map(([a]) => [a, 0]),
+        ),
+    };
+    console.log(resetedData);
+    const [animatedData, setAnimatedData] =
+        React.useState<VoteTotal>(resetedData);
+
+    React.useEffect(() => {
+        return;
+    }, [animatedData]);
+
+    const chartData = makeChartData(animatedData);
+
+    const weEnded = currentIndex === Object.keys(data.count).length;
+    const revealNext = () => {
+        if (weEnded) return;
+
+        const newData = {
+            ...animatedData,
+            count: Object.fromEntries(
+                Object.entries(animatedData.count).map(([a, b], index) =>
+                    index === currentIndex
+                        ? [a, Object.entries(data.count)[index][1]]
+                        : [a, b],
+                ),
+            ),
+        };
+
+        setAnimatedData(newData);
+        setCurrentIndex((e) => e + 1);
+    };
+
     return (
         <div>
-            <Bar type="Bar" data={data} options={options} />
+            <h2 className="mb-8 text-5xl font-bold text-center text-black text-opacity-80 ">
+                {data.position}
+            </h2>
+
+            <button
+                disabled={weEnded}
+                onClick={revealNext}
+                className={clsx(
+                    weEnded ? "bg-blue-200 cursor-not-allowed " : "bg-blue-500",
+                    "flex p-3 font-bold text-white rounded shadow ",
+                )}
+            >
+                Reveal Next !
+            </button>
+            <Bar type="Bar" data={chartData} options={options} />
         </div>
     );
 };
